55df3ed56105374d68cdc40dce4beb8a27c6eab2
Sergey Boykov <sergsv03@gmail.com>
2021-12-10 21:30:04 +0100

    prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,98 +1,97 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
 class CustomElement extends HTMLElement {
   constructor() {
-    super();
+    super()
   }
 
   connectedCallback() {
-    console.log("connectedCallback", this);
-    const options = typeof App === "function" ? App.options : App;
+    console.log('connectedCallback', this)
+    const options = typeof App === 'function' ? App.options : App
     const propsList: string[] = Array.isArray(options.props)
       ? options.props
-      : Object.keys(options.props || {});
+      : Object.keys(options.props || {})
 
-    const props: { [index: string]: string } = {};
+    const props: { [index: string]: string } = {}
     // Validate, if all props are present
     for (const prop of propsList) {
       const propValue =
-        process.env.NODE_ENV === "development"
+        process.env.NODE_ENV === 'development'
           ? process.env[`VUE_APP_${prop.toUpperCase()}`]
-          : this.attributes.getNamedItem(prop)?.value;
+          : this.attributes.getNamedItem(prop)?.value
 
       if (!propValue) {
-        console.error(`Missing attribute ${prop}`);
-        return;
+        console.error(`Missing attribute ${prop}`)
+        return
       }
 
-      props[prop] = propValue;
+      props[prop] = propValue
     }
 
-    const app = createApp(App, props);
-    console.log("props", props, app);
+    const app = createApp(App, props)
+    console.log('props', props, app)
 
-    app.use(router);
+    app.use(router)
 
-    const root = this.attachShadow({ mode: "open" });
+    const root = this.attachShadow({ mode: 'open' })
 
-    const styleLink = document.createElement("link");
-    styleLink.setAttribute("href", "css/app.css");
-    styleLink.setAttribute("rel", "stylesheet");
-    root.appendChild(styleLink);
+    const styleLink = document.createElement('link')
+    styleLink.setAttribute('href', 'css/app.css')
+    styleLink.setAttribute('rel', 'stylesheet')
+    root.appendChild(styleLink)
 
-    const appMountPoint = document.createElement("div");
-    root.appendChild(appMountPoint);
+    const appMountPoint = document.createElement('div')
+    root.appendChild(appMountPoint)
 
     waitForCssLoaded(styleLink).then((loaded) => {
       if (loaded) {
-        app.mount(appMountPoint);
+        app.mount(appMountPoint)
       } else {
-        console.error("Can't load css: css/app.css");
+        console.error("Can't load css: css/app.css")
       }
-    });
+    })
   }
 }
 
 function isCssLinkLoaded(linkElem: HTMLLinkElement) {
-  let cssLoaded = false;
+  let cssLoaded = false
   try {
     if ((linkElem?.sheet?.cssRules?.length ?? 0) > 0) {
-      cssLoaded = true;
+      cssLoaded = true
     } else if ((linkElem?.innerHTML?.length ?? 0) > 0) {
-      cssLoaded = true;
+      cssLoaded = true
     }
   } catch (ex) {
-    cssLoaded = false;
+    cssLoaded = false
   }
-  return cssLoaded;
+  return cssLoaded
 }
 
 async function waitForCssLoaded(linkElem: HTMLLinkElement) {
   if (isCssLinkLoaded(linkElem)) {
-    return true;
+    return true
   }
-  const pollPeriodicityMs = 50;
-  const maxWaitTimeMs = 10_000;
-  const maxAttempts = Math.ceil(maxWaitTimeMs / pollPeriodicityMs);
-  let attemptCount = 0;
+  const pollPeriodicityMs = 50
+  const maxWaitTimeMs = 10_000
+  const maxAttempts = Math.ceil(maxWaitTimeMs / pollPeriodicityMs)
+  let attemptCount = 0
 
   return new Promise<boolean>((resolve) => {
     const poller = setInterval(() => {
-      attemptCount++;
+      attemptCount++
       if (isCssLinkLoaded(linkElem)) {
-        clearTimeout(poller);
-        return setTimeout(() => resolve(true), 10000);
-        // return resolve(true);
+        clearTimeout(poller)
+        return resolve(true)
       }
       if (attemptCount > maxAttempts) {
-        clearTimeout(poller);
-        return resolve(false);
+        clearTimeout(poller)
+        return resolve(false)
       }
-    });
-  });
+    })
+  })
 }
 
-window.customElements.define("wc-test", CustomElement); // for prod only
+window.customElements.define('wc-test', CustomElement) // for prod only
 // createApp(App).use(router).mount("#app"); // for dev
